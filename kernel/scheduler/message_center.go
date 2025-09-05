aec09fb78140bf2a0fdffb3604499f9d4e1955f2
wb_liubeiluo <wb_liubeiluo@kuaishou.com>
2025-09-05 22:31:32 +0800

    refactor(message_center): 重构消息中心代码 - 对kernel/scheduler/message_center.go文件中的代码进行了重构，有新增和删除代码操作 [kernel/scheduler/message_center.go]

--- kernel/scheduler/message_center.go
+++ kernel/scheduler/message_center.go
@@ -1,15 +1,7 @@
 package scheduler
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
 	"github.com/nats-io/nats.go"
-	"github.com/yunhanshu-net/function-go/pkg/dto/request"
-	"github.com/yunhanshu-net/function-runtime/conf"
-	"github.com/yunhanshu-net/pkg/constants"
-	"github.com/yunhanshu-net/pkg/dto/runnerproject"
-	"github.com/yunhanshu-net/pkg/logger"
 )
 
 var natsConn *nats.Conn
@@ -35,93 +27,93 @@ func PushFunctionServer(msg *nats.Msg) error {
 }
 
 // PushRunner 消息推送给runner
-func PushRunner(runnerRequest *request.RunFunctionReq, runner *runnerproject.Runner) error {
-	sub := fmt.Sprintf("runner.%s.%s.%s.run", runner.User, runner.Name, runner.Version)
-	msg := nats.NewMsg(sub)
-	runnerRequest.Runner = runner
-	marshal, err := json.Marshal(runnerRequest)
-	if err != nil {
-		return err
-	}
-	msg.Data = marshal
-	msg.Header.Set(constants.TraceID, runnerRequest.TraceID)
-	return getConn().PublishMsg(msg)
-}
-
-// OnSubFunctionServer 接收到FunctionServer的消息后推送消息处理
-func OnSubFunctionServer(msg *nats.Msg) {
-
-	ctx := context.WithValue(context.Background(), constants.TraceID, msg.Header.Get(constants.TraceID))
-	//logger.Infof(ctx, "function.run >%s uid:%s", msg.Subject, string(msg.Data))
-	//接收runner关闭
-	logger.Infof(ctx, "Got message: %s", string(msg.Data))
-	runner, err := runnerproject.NewRunner(msg.Header.Get("user"), msg.Header.Get("runner"), conf.GetRunnerRoot(), msg.Header.Get("version"))
-	if err != nil {
-		panic(err)
-	}
-
-	req := request.RunFunctionReq{
-		Method:  msg.Header.Get("method"),
-		Router:  msg.Header.Get("router"),
-		TraceID: msg.Header.Get(constants.TraceID),
-		Runner:  runner,
-		//BodyType: "string",
-	}
-	if req.IsMethodGet() {
-		req.UrlQuery = msg.Header.Get("url_query")
-	} else {
-		req.Body = string(msg.Data)
-		req.BodyType = "string"
-	}
-	rsp := nats.NewMsg(msg.Subject)
-	rsp.Header.Set("code", "0")
-	//推送消息让runner处理
-	err = scheduler.RequestSync(ctx, &req)
-	if err != nil {
-		panic(err)
-	}
-
-}
-
-// SubFunctionServer function-runtime接收来自function-server的消息的主题
-func SubFunctionServer(handler nats.MsgHandler) error {
-	subscribe, err := getConn().Subscribe("function-server.sub", handler)
-	if err != nil {
-		return err
-	}
-	subFunctionServer = subscribe
-	return nil
-}
-
-// OnSubRunner 接收到runner的消息后转发给FunctionServer
-func OnSubRunner(msg *nats.Msg) {
-	err := PushFunctionServer(msg)
-	if err != nil {
-		panic(err)
-	}
-}
-
-// SubRunner function-runtime接收来自function-server的消息的主题
-func SubRunner(handler nats.MsgHandler) error {
-	subscribe, err := getConn().Subscribe("function-runner.sub", handler)
-	if err != nil {
-		return err
-	}
-	subRunner = subscribe
-	return nil
-}
-
-func Close() {
-	if subRunner != nil {
-		subRunner.Unsubscribe()
-	}
-	if subFunctionServer != nil {
-		subFunctionServer.Unsubscribe()
-	}
-	if natsConn != nil {
-		natsConn.Close()
-	}
-}
+//func PushRunner(runnerRequest *request.RunFunctionReq, runner *runnerproject.Runner) error {
+//	sub := fmt.Sprintf("runner.%s.%s.%s.run", runner.User, runner.Name, runner.Version)
+//	msg := nats.NewMsg(sub)
+//	runnerRequest.Runner = runner
+//	marshal, err := json.Marshal(runnerRequest)
+//	if err != nil {
+//		return err
+//	}
+//	msg.Data = marshal
+//	msg.Header.Set(constants.TraceID, runnerRequest.TraceID)
+//	return getConn().PublishMsg(msg)
+//}
+//
+//// OnSubFunctionServer 接收到FunctionServer的消息后推送消息处理
+//func OnSubFunctionServer(msg *nats.Msg) {
+//
+//	ctx := context.WithValue(context.Background(), constants.TraceID, msg.Header.Get(constants.TraceID))
+//	//logger.Infof(ctx, "function.run >%s uid:%s", msg.Subject, string(msg.Data))
+//	//接收runner关闭
+//	logger.Infof(ctx, "Got message: %s", string(msg.Data))
+//	runner, err := runnerproject.NewRunner(msg.Header.Get("user"), msg.Header.Get("runner"), conf.GetRunnerRoot(), msg.Header.Get("version"))
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	req := request.RunFunctionReq{
+//		Method:  msg.Header.Get("method"),
+//		Router:  msg.Header.Get("router"),
+//		TraceID: msg.Header.Get(constants.TraceID),
+//		Runner:  runner,
+//		//BodyType: "string",
+//	}
+//	if req.IsMethodGet() {
+//		req.UrlQuery = msg.Header.Get("url_query")
+//	} else {
+//		req.Body = string(msg.Data)
+//		req.BodyType = "string"
+//	}
+//	rsp := nats.NewMsg(msg.Subject)
+//	rsp.Header.Set("code", "0")
+//	//推送消息让runner处理
+//	err = scheduler.RequestSync(ctx, &req)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//}
+//
+//// SubFunctionServer function-runtime接收来自function-server的消息的主题
+//func SubFunctionServer(handler nats.MsgHandler) error {
+//	subscribe, err := getConn().Subscribe("function-server.sub", handler)
+//	if err != nil {
+//		return err
+//	}
+//	subFunctionServer = subscribe
+//	return nil
+//}
+//
+//// OnSubRunner 接收到runner的消息后转发给FunctionServer
+//func OnSubRunner(msg *nats.Msg) {
+//	err := PushFunctionServer(msg)
+//	if err != nil {
+//		panic(err)
+//	}
+//}
+//
+//// SubRunner function-runtime接收来自function-server的消息的主题
+//func SubRunner(handler nats.MsgHandler) error {
+//	subscribe, err := getConn().Subscribe("function-runner.sub", handler)
+//	if err != nil {
+//		return err
+//	}
+//	subRunner = subscribe
+//	return nil
+//}
+//
+//func Close() {
+//	if subRunner != nil {
+//		subRunner.Unsubscribe()
+//	}
+//	if subFunctionServer != nil {
+//		subFunctionServer.Unsubscribe()
+//	}
+//	if natsConn != nil {
+//		natsConn.Close()
+//	}
+//}
 
 type MessageCenter struct {
 	natsConn *nats.Conn
